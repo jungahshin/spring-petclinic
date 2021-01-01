17b9bcf858a788f09a354e43c9356c31fee1f389
jungahshin <shinasong@naver.com>
2021-01-02 00:05:18 +0900

    [ADD]SampleController/SampleConfig/SampleControllerTest

--- src/main/java/org/springframework/samples/petclinic/sample/SampleController.java
+++ src/main/java/org/springframework/samples/petclinic/sample/SampleController.java
@@ -1,10 +1,17 @@
 package org.springframework.samples.petclinic.sample;
 
+import org.springframework.stereotype.Controller;
+
+//@Controller
 public class SampleController {
 	SampleRepository sampleRepository;
 
 	public SampleController(SampleRepository sampleRepository) {
 		this.sampleRepository = sampleRepository;
+	}
+
+	public SampleController() {
+
 	}
 
 	public void doSomething(){
--- src/test/java/org/springframework/samples/petclinic/sample/SampleControllerTest.java
+++ src/test/java/org/springframework/samples/petclinic/sample/SampleControllerTest.java
@@ -2,8 +2,6 @@ package org.springframework.samples.petclinic.sample;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class SampleControllerTest {
 	// SampleControllerTest SampleController(생성자?)에 의존성을 주입한다.
 
@@ -12,6 +10,7 @@ class SampleControllerTest {
 	{
 		SampleRepository sampleRepository =  new SampleRepository();
 		SampleController sampleController = new SampleController(sampleRepository);
+
 		// 이미 생성자를 통해 sampleRepository가 만들어져서
 		// doSomething메소드(sampleRepository.save()실행)를 실행해도 NullPtr exception이 발생하지 않는다.
 		sampleController.doSomething();
